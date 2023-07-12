51bff736594137dbe74ab83d79d85019cab080f6
tig0826 <tigcr0826@gmail.com>
2023-07-12 14:00:57 +0900

    add ipynb

--- my_snowpark_streamlit_app.py
+++ my_snowpark_streamlit_app.py
@@ -1,5 +1,5 @@
 from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col lit
+from snowflake.snowpark.functions import avg, sum, col, lit
 import streamlit as st
 import pandas as pd
 
